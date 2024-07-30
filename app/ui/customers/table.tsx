a9f10d25dbdaa8200cc2a838838862e46d7ecc0d
Paavo <paavo.nguyen@gmail.com>
2024-07-31 00:33:29 +0300

    typo

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -28,7 +28,7 @@ export default async function CustomersTable({
                         <div className='flex items-center gap-3'>
                           <Image
                             src={customer.image_url}
-                            className='rounded-ful aspect-square object-contain'
+                            className='rounded-full aspect-square object-contain'
                             alt={`${customer.name}'s profile picture`}
                             width={28}
                             height={28}
